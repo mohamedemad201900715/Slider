04ffb001aa0ab1cdbbbb87e895edf1c68681ec69
mohamedemad201900715 <medoem82950@gmail.com>
2023-08-02 16:20:42 +0200

    Edit Js final

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
+const slider = document.querySelector('.slider');
+const slides = slider.getElementsByTagName('img');
+const right = document.querySelector('.arrow-right')
+const left = document.querySelector('.arrow-left')
+const sliderBottom = document.querySelectorAll(".slide-bottom");
 (function createSlider() {
-    const slider = document.querySelector('.slider');
-    const slides = slider.getElementsByTagName('img');
-    const right = document.querySelector('.arrow-right')
-    const left = document.querySelector('.arrow-left')
-    const sliderBottom = document.querySelector(".slides-bottom").getElementsByTagName("div");
-
     let currentIndex = 0;
+
 
     function showSlide(index) {
       for (let i = 0; i < slides.length; i++) {
@@ -15,15 +15,33 @@
       sliderBottom[currentIndex].classList.add('active');
       slides[index].style.display = 'block';
     }
+    
+
     right.onclick = function (){
       currentIndex = (currentIndex + 1) % slides.length;
       showSlide(currentIndex)
     }
+    
+
     left.onclick = function (){
       currentIndex = (currentIndex - 1 + slides.length) % slides.length;
       showSlide(currentIndex);
       
     }
+    
+
+    sliderBottom.forEach((dot, index) => {
+      dot.addEventListener('click', function(){
+         goToSlide(index);
+        });
+    });
+
+
+    function goToSlide(slideIndex) {
+      currentIndex = slideIndex;
+      showSlide(currentIndex);
+    }
+
 
         showSlide(currentIndex);
 
